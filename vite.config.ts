67b88df157712083586165980ded9e9a21b0403c
ZYROUGE <zyrouge@hotmail.com>
2021-05-19 21:05:18 +0530

    Several fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const isElectronBuild = process.env.BUILD_MODE === "electron";
+
 export default defineConfig({
+    base: isElectronBuild ? "./" : "/",
     plugins: [vue()],
     build: {
         outDir: "dist/vite",
