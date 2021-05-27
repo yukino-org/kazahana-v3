cb39e1adaf8978ab7798db5746b41f6ec62009ef
ZYROUGE <zyrouge@hotmail.com>
2021-05-27 16:56:40 +0530

    Minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import html from "vite-plugin-html";
 import { version } from "./package.json";
 
 const platform = process.env.YUKINO_PLATFORM || "unknown";
 
 export default defineConfig({
-    base: ["electron"].includes(platform) ? "./" : "/",
-    plugins: [vue()],
+    base: getBase(platform),
+    plugins: [
+        vue(),
+        html({
+            inject: {
+                injectData: {
+                    scripts: getScripts(platform),
+                },
+            },
+            minify: true,
+        }),
+    ],
     build: {
         outDir: "dist/vite",
     },
@@ -15,3 +26,23 @@ export default defineConfig({
         app_version: `"${version}"`,
     },
 });
+
+function getBase(platform: string) {
+    switch (platform) {
+        case "electron":
+            return "./";
+
+        default:
+            return "/";
+    }
+}
+
+function getScripts(platform: string) {
+    let scripts: string[] = [];
+
+    if (platform === "capacitor") {
+        // scripts.push();
+    }
+
+    return scripts.join("\n");
+}
