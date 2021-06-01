80902b6718be20c4d7279cb02c67e213e1b44c55
ZYROUGE <zyrouge@hotmail.com>
2021-06-01 20:44:31 +0530

    Fixed release script

--- scripts/capacitor-android-debug-release.js
+++ scripts/capacitor-android-debug-release.js
@@ -2,7 +2,7 @@ const builder = require("./utils/capacitor-android-build");
 
 const start = async () => {
     const out = await builder("release");
-    console.log(`Output apk can be found at: ${release}`);
+    console.log(`Output apk can be found at: ${out}`);
 };
 
 start();
