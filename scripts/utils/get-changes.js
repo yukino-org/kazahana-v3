653da57c8503fbbf4059f03ac865b56f2ebed20d
ZYROUGE <zyrouge@hotmail.com>
2021-06-10 10:46:49 +0530

    ci: use github api

--- scripts/utils/get-changes.js
+++ scripts/utils/get-changes.js
@@ -1,16 +1,23 @@
-const cp = require("child_process");
-const util = require("util");
+const { compare } = require("@zyrouge/git-compare");
 
-const exec = util.promisify(cp.exec);
-const filterRegex =
-    /^\b[0-9a-f]{5,40}\b (build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([\w-\.]+\))?(!)?: ([\w ])+([\s\S]*)/;
-
-module.exports = async (tag1, tag2) => {
-    const { stdout } = await exec(`git rev-list --oneline ${tag1}..${tag2}`);
-    if (!stdout) return null;
-    return [
-        ...new Set(stdout.split("\n").filter((l) => filterRegex.test(l))),
-    ].join("\n");
+module.exports = async (opts) => {
+    const changes = await compare({
+        ...opts,
+        allowOnlyConventional: true,
+        filterDuplicatesMsg: true,
+    });
+    return changes.map((x) => `${x.id} ${x.msg}`).join("\n");
 };
 
-// module.exports("v0.0.8-beta.0", "v0.0.10-beta.0").then(console.log);
+// module
+//     .exports({
+//         repo: {
+//             author: "zyrouge",
+//             repo: "yukino-app",
+//         },
+//         version: {
+//             previous: "v0.0.8-beta.0",
+//             current: "v0.0.10-beta.0",
+//         },
+//     })
+//     .then(console.log);
