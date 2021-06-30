5721aa486fda3a5b6f61a57b2e3278af2d76de0f
ZYROUGE <zyrouge@hotmail.com>
2021-06-30 17:47:37 +0530

    refactor: typed router-view & router-link

--- src/components.d.ts
+++ src/components.d.ts
@@ -1,9 +1,12 @@
 import { DefineComponent } from "vue";
+import { RouteComponent, RouterView } from "vue-router";
 import { FontAwesomeIconProps } from "@fortawesome/vue-fontawesome";
 
 declare module "vue" {
     export interface GlobalComponents {
         Icon: DefineComponent<FontAwesomeIconProps>;
+        "router-link": RouteComponent;
+        "router-view": typeof RouterView;
     }
 }
 
