17f8f8525bb9b5da0df3bae9cd4933dea6aa1f0e
ZYROUGE <zyrouge@hotmail.com>
2021-09-21 11:57:03 +0530

    ci: app to ipa

--- cli/flutter-cli/build/ios/index.ts
+++ cli/flutter-cli/build/ios/index.ts
@@ -1,29 +1,28 @@
-import { dirname, join } from "path";
-import { ensureDir, copyFile } from "fs-extra";
+import { basename, join } from "path";
+import { promisify } from "util";
+import AdmZip from "adm-zip";
 import { spawn, promisifyChildProcess } from "../../../spawn";
 import { config } from "../../../config";
 import { Logger } from "../../../logger";
 import { getVersion } from "../../../helpers/version";
 
-export const built = join(config.base, "build/ios/ipa/yukino_app.ipa");
+export const built = join(config.base, "build/ios/iphoneos/Runner.app");
 
 const logger = new Logger("build:ios");
 
 export const build = async () => {
     await promisifyChildProcess(
-        await spawn(
-            "flutter",
-            ["build", "ipa", "--release", "--no-codesign"],
-            config.base
-        )
+        await spawn("flutter", ["build", "ios", "--no-codesign"], config.base)
     );
+
+    const zip = new AdmZip();
+    zip.addLocalFile(built);
 
     const out = join(
         config.ios.packed,
         `${config.name} v${await getVersion()} Installer.ipa`
     );
-    await ensureDir(dirname(out));
-    await copyFile(built, out);
+    await promisify(zip.writeZip)(out);
 
     logger.log(`Installer created: ${out}`);
 };
