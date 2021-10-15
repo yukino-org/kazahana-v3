8fe8ee548e04943ce2c2b8b63bc8299675aa589c
ZYROUGE <zyrouge@hotmail.com>
2021-10-15 12:36:07 +0530

    ci: upload release builds

--- cli/config.ts
+++ cli/config.ts
@@ -1,9 +1,15 @@
 import { resolve, join } from "path";
 
+const pkgJson = require("../package.json");
+
 const root = resolve(__dirname, "..");
 
 const defaultIcon = join(root, "assets/images/yukino-icon.png");
 const buildDir = join(root, "build", "packed");
+
+const githubMatch = /^git\+https:\/\/github\.com\/([^/]+)\/([^.]+)\.git$/.exec(
+    pkgJson.repository.url
+);
 
 export const config = {
     name: "Yukino",
@@ -34,5 +40,9 @@ export const config = {
         project: join(root, "windows"),
         icon: defaultIcon,
         packed: join(buildDir, "windows"),
+    },
+    github: {
+        username: githubMatch![1],
+        repo: githubMatch![1],
     },
 };
---
+++ cli/helpers/check-release/index.ts
@@ -0,0 +1,46 @@
+import got, { RequestError as GotRequestError } from "got";
+import commandLineArgs from "command-line-args";
+import { config } from "../../config";
+import { Logger } from "../../logger";
+import { getVersion } from "../version";
+
+const logger = new Logger("check-release");
+
+const argsOpts: commandLineArgs.OptionDefinition[] = [
+    {
+        name: "ends",
+        alias: "e",
+        type: String,
+        multiple: true,
+    },
+];
+
+export const checkRelease = async () => {
+    const { ends } = commandLineArgs(argsOpts);
+
+    if (!Array.isArray(ends) || !ends.length) {
+        throw new Error("Missing argument '-e'");
+    }
+
+    const version = await getVersion();
+    const res = await got
+        .get(
+            `https://api.github.com/repos/${config.github.username}/${config.github.repo}/releases/tags/v${version}`
+        )
+        .catch((e) => (e as GotRequestError).response);
+
+    if (res?.statusCode == 200) {
+        const parsed = JSON.parse(res.body as string);
+        const matches = (parsed.assets as any[]).some((x) =>
+            (ends as string[]).some((y: string) => (<string>x.name).endsWith(y))
+        );
+
+        if (matches) {
+            throw new Error(
+                `Matches in tag v${version} were found. Please remove them before building.`
+            );
+        }
+    }
+
+    logger.log(`Tag v${version} does not exist, proceeding...`);
+};
