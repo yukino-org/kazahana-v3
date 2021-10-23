0ff6d1ec3fa1251153398f29027798d177da06f3
ZYROUGE <zyrouge@hotmail.com>
2021-10-23 13:05:25 +0530

    ci: generate `changelogs.json`

--- cli/others/changelogs/index.ts
+++ cli/others/changelogs/index.ts
@@ -1,17 +1,61 @@
+import { join } from "path";
+import { ensureDir, writeFile } from "fs-extra";
 import got from "got";
 import { getOctokit } from "@actions/github";
+import { Endpoints } from "@octokit/types";
 import { config } from "../../config";
 import { Logger } from "../../logger";
 import { capitalizeString } from "../../utils";
 
 const logger = new Logger("actions:update-changelogs");
 
-interface Commits {
-    feat: string[];
-    refactor: string[];
-    fix: string[];
-    perf: string[];
+type GitHubRelease =
+    Endpoints["GET /repos/{owner}/{repo}/releases/latest"]["response"]["data"];
+type GitHubRefCompare =
+    Endpoints["GET /repos/{owner}/{repo}/compare/{base}...{head}"]["response"]["data"];
+
+class CommitMessage {
+    constructor(
+        public readonly commit: {
+            type: "feat" | "refactor" | "fix" | "perf";
+            cat?: string;
+            msg: string;
+            stats: {
+                additions: number;
+                deletions: number;
+            };
+            sha: string;
+            url: string;
+        }
+    ) {}
+
+    isSame(other: CommitMessage) {
+        return (
+            this.commit.type === other.commit.type ||
+            this.commit.cat === other.commit.cat ||
+            this.commit.msg === other.commit.msg
+        );
+    }
+
+    toString() {
+        const chunks = [
+            `[\`${this.commit.sha.slice(0, 6)}\`](${this.commit.url})`,
+        ];
+        if (this.commit.cat) {
+            chunks.push(`**${this.commit.cat}**:`);
+        }
+        chunks.push(capitalizeString(this.commit.msg));
+        chunks.push(
+            `+${this.commit.stats.additions} -${this.commit.stats.deletions}`
+        );
+
+        return chunks.join(" ");
+    }
 }
+
+type Commits = {
+    [k in CommitMessage["commit"]["type"]]: CommitMessage[];
+};
 
 class Changelogs {
     commits: Commits = {
@@ -22,12 +66,43 @@ class Changelogs {
     };
 
     constructor(
-        public readonly downloadURL: string,
-        public readonly releaseURL: string
-    ) {}
+        public readonly release: GitHubRelease,
+        public readonly diff: GitHubRefCompare
+    ) {
+        for (const x of this.diff.commits) {
+            const match =
+                /^(feat|fix|perf|refactor){1}(\(([\w-\.]+)\))?(!)?: (.*)/.exec(
+                    x.commit.message
+                );
+
+            if (match) {
+                const msg = new CommitMessage({
+                    type: match[1] as keyof Commits,
+                    cat: match[3]
+                        ? capitalizeString(
+                              match[3].replace(/_[a-z]/, (str) =>
+                                  str.slice(1).toUpperCase()
+                              )
+                          )
+                        : undefined,
+                    msg: capitalizeString(match[5]),
+                    sha: x.sha.slice(0, 6),
+                    url: x.html_url,
+                    stats: {
+                        additions: x.stats?.additions ?? 0,
+                        deletions: x.stats?.deletions ?? 0,
+                    },
+                });
+
+                if (!this.commits[msg.commit.type].some((x) => x.isSame(msg))) {
+                    this.commits[msg.commit.type].push(msg);
+                }
+            }
+        }
+    }
 
     getDiscordMessageBody() {
-        return `**Download**: ${this.downloadURL}\n**Release**: ${this.releaseURL}`;
+        return `**Download**: ${this.downloadURL}\n**Release**: ${this.release.html_url}`;
     }
 
     getGithubReleaseBody(body: string) {
@@ -42,7 +117,7 @@ class Changelogs {
     }
 
     getLinks() {
-        return `## Links\n  - Download: ${this.downloadURL}\n   - Release: ${this.releaseURL}`;
+        return `## Links\n  - Download: ${this.downloadURL}\n   - Release: ${this.release.html_url}`;
     }
 
     getChangelogs() {
@@ -50,7 +125,7 @@ class Changelogs {
 
         for (const [k, v] of Object.entries(this.commits) as [
             keyof Commits,
-            string[]
+            CommitMessage[]
         ][]) {
             if (v.length) {
                 let head: string;
@@ -82,7 +157,17 @@ class Changelogs {
 
         return logs.join("\n");
     }
+
+    get json() {
+        return JSON.stringify(this);
+    }
+
+    get downloadURL() {
+        return `${config.url}/download/${this.release.tag_name}`;
+    }
 }
+
+const repo = { owner: config.github.username, repo: config.github.repo };
 
 export const updateChangelogs = async (
     githubToken: string,
@@ -91,19 +176,15 @@ export const updateChangelogs = async (
     logger.log("Generating changelogs");
 
     const github = getOctokit(githubToken, {});
-    const repo = { owner: config.github.username, repo: config.github.repo };
 
-    const res = await github.request("GET /repos/{owner}/{repo}/releases", {
+    const {
+        data: [latest, previous],
+    } = await github.request("GET /repos/{owner}/{repo}/releases", {
         ...repo,
         per_page: 2,
     });
 
-    const [previous, latest] = res.data.sort(
-        (a, b) =>
-            new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
-    );
-
-    const diff = await github.request(
+    const { data: diff } = await github.request(
         "GET /repos/{owner}/{repo}/compare/{base}...{head}",
         {
             ...repo,
@@ -112,49 +193,24 @@ export const updateChangelogs = async (
         }
     );
 
-    const changelogs = new Changelogs(
-        `${config.url}/download/${latest.tag_name}`,
-        latest.html_url
-    );
+    const changelogs = new Changelogs(latest, diff);
 
-    for (const x of diff.data.commits) {
-        const match =
-            /^(feat|fix|perf|refactor){1}(\(([\w-\.]+)\))?(!)?: (.*)/.exec(
-                x.commit.message
-            );
-
-        if (match) {
-            const chunks = [`[\`${x.sha.slice(0, 6)}\`](${x.html_url})`];
-            if (match[3]) {
-                chunks.push(
-                    `**${capitalizeString(
-                        match[3].replace(/_[a-z]/, (str) =>
-                            str.slice(1).toUpperCase()
-                        )
-                    )}**:`
-                );
-            }
-            chunks.push(capitalizeString(match[5]));
-            if (x.stats?.additions || x.stats?.deletions) {
-                chunks.push(
-                    `+${x.stats.additions || 0} -${x.stats.deletions || 0}`
-                );
-            }
-
-            const msg = chunks.join(" ");
-            const key = match[1] as keyof Commits;
-            if (!changelogs.commits[key].includes(msg)) {
-                changelogs.commits[key].push(msg);
-            }
-        }
-    }
+    await ensureDir(config.cacheDir);
+    await writeFile(join(config.cacheDir, "changelogs.json"), changelogs.json);
+    logger.log("Created changelogs.json");
 
     await github.request("POST /repos/{owner}/{repo}/releases/{release_id}", {
         ...repo,
         release_id: latest.id,
-        body: changelogs.getGithubReleaseBody(latest.body || ""),
+        body: changelogs.getGithubReleaseBody(latest.body ?? ""),
     });
+    logger.log("Updated release");
 
+    await github.request("POST /repos/{owner}/{repo}/releases/{release_id}", {
+        ...repo,
+        release_id: latest.id,
+        body: changelogs.getGithubReleaseBody(latest.body ?? ""),
+    });
     logger.log("Updated release");
 
     await got.post(discordWebhookURL, {
@@ -179,6 +235,5 @@ export const updateChangelogs = async (
             ],
         }),
     });
-
     logger.log("Posted webhook");
 };
