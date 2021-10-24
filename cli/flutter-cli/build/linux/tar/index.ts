065c0c74865271afc23ded79b6b58d17a86332a5
ZYROUGE <zyrouge@hotmail.com>
2021-10-24 12:06:50 +0530

    chore(build): add tar.gz for linux

---
+++ cli/flutter-cli/build/linux/tar/index.ts
@@ -0,0 +1,32 @@
+import { dirname, join } from "path";
+import { ensureDir, createWriteStream } from "fs-extra";
+import tar from "tar";
+import { getVersion } from "../../../../others/version/print";
+import { Logger } from "../../../../logger";
+import { config } from "../../../../config";
+import { buildDir } from "../";
+
+const logger = new Logger("linux:build:tar.gz");
+
+export const build = async () => {
+    const version = await getVersion();
+
+    const finalPath = join(
+        config.linux.packed,
+        `${config.name}_v${version}-linux.tar.gz`
+    );
+
+    await ensureDir(dirname(finalPath));
+
+    const stream = createWriteStream(finalPath);
+    tar.create(
+        {
+            gzip: true,
+            cwd: buildDir,
+        },
+        [buildDir]
+    ).pipe(stream);
+    await new Promise((resolve) => stream.once("close", resolve));
+
+    logger.log(`Tarball created: ${finalPath}`);
+};
