4622802fce6dbed52d0d6944dd4b009add4db91d
ZYROUGE <zyrouge@hotmail.com>
2021-11-07 11:58:45 +0530

    chore(extensions): fix exports

--- packages/extensions/lib/extensions.dart
+++ packages/extensions/lib/extensions.dart
@@ -1,6 +1,10 @@
 library extensions;
 
+export './core/base.dart';
 export './core/extension.dart';
+export './core/resolvable.dart';
+export './core/resolved.dart';
+export './core/version.dart';
 export './models/anime.dart';
 export './models/base.dart';
 export './models/manga.dart';
