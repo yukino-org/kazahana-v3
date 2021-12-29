87893f7268d443c7b6a31decfac7fdf6d1964086
ZYROUGE <zyrouge@hotmail.com>
2021-12-29 15:31:05 +0530

    chore: try fix cloudflare bypasser

--- packages/extensions/lib/utils/html_dom/utils.dart
+++ packages/extensions/lib/utils/html_dom/utils.dart
@@ -1,25 +1,23 @@
 import './html_dom.dart';
 
 abstract class HtmlDOMUtils {
-  static const Duration _cloudflareCheckDuration = Duration(seconds: 6);
-  static const Duration _cloudflarePostCheckDuration = Duration(seconds: 1);
+  static const Duration cloudflareCheckDuration = Duration(seconds: 6);
 
-  static bool _isItCloudflareCheck(final String html) {
-    final String _html = html.toLowerCase();
+  static bool isBlockedByCloudflare(final String html) {
+    final String htmlLwr = html.toLowerCase();
 
     return <String>[
       'id="cf-wrapper"',
-      'checking your browser',
-    ].every((final String x) => _html.contains(x));
+    ].every((final String x) => htmlLwr.contains(x));
   }
 
   static Future<bool> tryBypassCloudflare(final HtmlDOMTab tab) async {
     final List<Duration> checkIntervals = <Duration>[
       Duration.zero,
-      _cloudflareCheckDuration,
+      cloudflareCheckDuration,
       ...List<Duration>.generate(
-        3,
-        (final int _) => _cloudflarePostCheckDuration,
+        5,
+        (final int i) => Duration(seconds: i + 1),
       ),
     ];
 
@@ -27,7 +25,7 @@ abstract class HtmlDOMUtils {
       await Future<void>.delayed(i);
 
       final String? html = await tab.getHtml();
-      if (html != null && !_isItCloudflareCheck(html)) {
+      if (html != null && !isBlockedByCloudflare(html)) {
         return true;
       }
     }
